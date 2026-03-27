7f4639ad24165cad9f549dbc9fdd1bfca19c9984
yqh <haihoing@qq.com>
2026-03-27 22:06:24 +0800

    移除 pythonx search

--- workspace/gen_data.py
+++ workspace/gen_data.py
@@ -3,22 +3,6 @@ import os, sys, re
 if not os.getcwd() in sys.path: # fix linux 软连接的 bug
     sys.path.append(os.getcwd())
 
-from pathlib import Path
-check_pythonx_dir = lambda x: x and os.path.exists(os.path.join(x, "funclib.py"))
-check_ksource_dir = lambda x: x and check_pythonx_dir(os.path.join(x, "pythonx"))
-for reldirx in (os.path.dirname(os.path.abspath(__file__)), # 这个先，规避加载漏洞。
-                os.path.abspath(".")):
-    while len(reldirx) > 3: # 3 应该所有平台都问题不大。
-        if check_ksource_dir(reldirx): break # YES!!
-        reldirx = os.path.dirname(reldirx) # 只尝试 funclib.py，funclib.pyc 存在版本问题。
-    if check_ksource_dir(reldirx): break # YES!!
-for devdirx in [r"E:\kSource", r"D:\kSource", r"C:\kSource"]:
-    if not check_ksource_dir(reldirx):
-        reldirx = devdirx
-if check_ksource_dir(reldirx) and not reldirx in sys.path:
-    sys.path.append(reldirx) # 放到最后，避免 sys.path.insert
-del devdirx # reldirx 可以继续使用
-
 from pythonx.funclib import *
 from pythonx.pelib import *
 from pythonx.mytoolspub import *
